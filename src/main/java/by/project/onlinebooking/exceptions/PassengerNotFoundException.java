9a1f48729ee015de7ba223fdfe07b798f42e597e
Krainyaya Maria <rizhik_masha23@mail,ru>
2020-04-06 14:53:25 +0300

    added passenger exception

---
+++ src/main/java/by/project/onlinebooking/exceptions/PassengerNotFoundException.java
@@ -0,0 +1,8 @@
+package by.project.onlinebooking.exceptions;
+
+public class PassengerNotFoundException extends RuntimeException {
+
+    public PassengerNotFoundException(Long id) {
+        super( "Could not find passenger " + id );
+    }
+}
