94fdfa573519038d05f66dbaf7d75fccecc63d5b
Krainyaya Maria <rizhik_masha23@mail,ru>
2020-04-06 14:39:16 +0300

    injected user service & updated urls

--- src/main/java/by/project/onlinebooking/controllers/UserController.java
+++ src/main/java/by/project/onlinebooking/controllers/UserController.java
@@ -1,12 +1,9 @@
 package by.project.onlinebooking.controllers;
 
 import by.project.onlinebooking.entities.User;
-import by.project.onlinebooking.exceptions.UserNotFoundException;
-import by.project.onlinebooking.repositories.PassengersRepository;
-import by.project.onlinebooking.repositories.UserRepository;
+import by.project.onlinebooking.services.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,58 +13,43 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/")
-@Api(value = "Online-booking System", description = "Operations pertaining to users and flights in Online-booking System")
+@Api(value = "Online-booking system")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
 
-    @Autowired
-    private PassengersRepository passengersRepository;
-
-    @ApiOperation(value = "View a list of all users", response = List.class)
-    @GetMapping("/admin/users")
-    public List<User> getAll() {
-        return userRepository.findAll();
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @ApiOperation(value = "Get a user by Id")
-    @GetMapping("/account/{id}")
+    @ApiOperation(value = "View a list of all users", response = List.class)
+    @GetMapping("/users")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
+    @ApiOperation(value = "Get a user by ID")
+    @GetMapping("/users/{id}")
     public User getUser(@PathVariable long id) {
-        return userRepository.findById( id )
-                .orElseThrow( () -> new UserNotFoundException( id ) );
+        return userService.getById( id );
     }
 
     @ApiOperation(value = "Add a user")
-    @PostMapping("/user")
-    public User newUser(@RequestBody User newUser) {
-        return userRepository.save( newUser );
+    @PostMapping("/users")
+    public User addUser(@Valid @RequestBody User user) {
+        return userService.add( user );
     }
 
     @ApiOperation(value = "Update a user")
-    @PutMapping("/account/{id}")
-    public User updateUser(
-            @PathVariable(value = "id") Long userId,
-            @Valid @RequestBody User userDetails)
-            throws UserNotFoundException {
-        User user = userRepository.findById( userId )
-                .orElseThrow( () -> new UserNotFoundException( userId ) );
-        user.setFirstName( userDetails.getFirstName() );
-        user.setLastName( userDetails.getLastName() );
-        user.setPassword( userDetails.getPassword() );
-        user.setPhone( userDetails.getPhone() );
-        return userRepository.save( user );
+    @PutMapping("/users")
+    public User updateUser(@Valid @RequestBody User user) {
+        return userService.update( user );
     }
 
     @ApiOperation(value = "Delete a user")
-    @DeleteMapping("/admin/users/{id}")
-    public void deleteUser(@PathVariable(value = "id") long userId)
-            throws UserNotFoundException {
-        passengersRepository.findAll().stream().filter( passenger -> passenger.getIdUser() == userId )
-                .forEach( passenger -> passengersRepository.delete( passenger ) );
-        User user = userRepository.findById( userId )
-                .orElseThrow( () -> new UserNotFoundException( userId ) );
-        userRepository.delete( user );
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable(value = "id") long id) {
+        userService.delete( id );
     }
 
 
