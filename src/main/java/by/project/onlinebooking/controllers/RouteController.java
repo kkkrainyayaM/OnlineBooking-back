f60b1e36a07962c9de477f8f654af110ef1567ad
Krainyaya Maria <rizhik_masha23@mail,ru>
2020-05-03 20:47:27 +0300

    initial date from liquibase

--- src/main/java/by/project/onlinebooking/controllers/RouteController.java
+++ src/main/java/by/project/onlinebooking/controllers/RouteController.java
@@ -6,7 +6,6 @@ import by.project.onlinebooking.services.RouteService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping(value = "/",
         consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
